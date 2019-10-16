a9afb6a31647dac384b7d5dd0fff3ba935940a21
Michael Stoll <michael.stoll@stolltec.com>
2019-10-16 14:35:51 +0200

    Calculate timer frequency

--- firmware/generator/Config/board.h
+++ firmware/generator/Config/board.h
@@ -2,10 +2,12 @@
 
 #ifndef BOARD_H_
 #define BOARD_H_
+#include <Config/peripheral_clk_config.h>
+#include <Config/hpl_rtc_config.h>
 
 #define FIRMWARE_VERSION (1)
 
-#define TIMER_FREQUENCY 62500
+#define TIMER_FREQUENCY (CONF_GCLK_RTC_FREQUENCY / (1 << CONF_RTC_PRESCALER))
 
 
 #endif /* BOARD_H_ */
