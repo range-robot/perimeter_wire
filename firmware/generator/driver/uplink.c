aa4d242473bc767ff259d65f7b0c01c8cc68b851
Michael Stoll <michael.stoll@stolltec.com>
2019-11-07 13:19:34 +0100

    generator: Initial configuration with enabled ch1, div 5 (till nv save is implemented)

--- firmware/generator/driver/uplink.c
+++ firmware/generator/driver/uplink.c
@@ -2,6 +2,18 @@
 #include <perimeter_wire_generator/registers.h>
 
 // hookup app layer
-uint8_t registers[REGISTER_COUNT] = {};
+uint8_t registers[REGISTER_COUNT] = {
+	0,  // REGISTER_CONTROL
+	0,  // REGISTER_TEMP
+	1,  // REGISTER_A_MODE
+	0,  // reserved
+	5,  // REGISTER_A_DIV
+	0,  // reserved
+	0, 	// REGISTER_B_MODE
+	0,  // reserved
+	0,  // REGISTER_B_DIV
+	0   // reserved
+};
+
 struct app_registers_t * app_registers = (struct app_registers_t*)registers;
 void (* const cmds[COMMAND_COUNT])(void)  = {};
