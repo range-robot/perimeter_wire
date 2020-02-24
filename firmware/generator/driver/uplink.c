9881f68f21a0812da0d92620dfda0b26cac8aeab
Michael Stoll <michael.stoll@meadow-robotics.com>
2020-02-24 21:54:40 +0100

    generator firmware: set default mode and code

--- firmware/generator/driver/uplink.c
+++ firmware/generator/driver/uplink.c
@@ -8,11 +8,11 @@ uint8_t registers[REGISTER_COUNT] = {
 	0x07,  // REGISTER_A_MODE
 	4,  // REGISTER_A_DIV
 	0x53,  // code
+	0x35,  // code
+	0x07, 	// REGISTER_B_MODE
+	4,  // REGISTER_B_DIV
 	0x53,  // code
-	0, 	// REGISTER_B_MODE
-	0,  // REGISTER_B_DIV
-	0,  // code
-	0   // code
+	0x35   // code
 };
 
 struct app_registers_t * app_registers = (struct app_registers_t*)registers;
